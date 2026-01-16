a5df692ac897d8c4c6597072d0887e9432f8723a
wheretogo33 <108924317+wheretogo33@users.noreply.github.com>
2026-01-16 12:12:40 +0900

    Update mtlcal.py

--- kspec_metrology/analysis/mtlcal.py
+++ kspec_metrology/analysis/mtlcal.py
@@ -2,13 +2,38 @@ import numpy as np
 from kspec_metrology.analysis.findpeak import findpeak
 from kspec_metrology.analysis.matchfiber import matchfiber
 from kspec_metrology.analysis.fitdistortion import fitdistortion
+from astropy.io import ascii
 
 def mtlcal(data_dir='./MTL/data/'):
     # Load Fiber positions and Fiducial flag------------------------------------------------
     #x, y, fid_flag = np.load(data_dir+"pos.npy")
     #fid_flag = fid_flag.astype(bool)
     
+    tab = ascii.read('Fiber_Configuration_250415.txt')
+    tab.rename_columns(tab.colnames[:4], ["ID", "X", "Y", "FiducialFlag"])
     
+    ids = [
+           'B5' , 'C2', 'D0',
+           'E5' , 'E8', 'G4',
+           'G11', 'H7', 'I2',
+           'I9' , 'K3', 'K6',
+           'L10', 'M7'
+          ]
+    
+    
+    fid_ids = tab["ID"][tab["FiducialFlag"] == 1]
+    pick_ids = list(dict.fromkeys(list(ids)) + list(fid_ids))
+    
+    #pick_ids.remove('Z0')
+    
+    xy_map = {r["ID"]: (r["X"], r["Y"]) for r in tab}
+    xy = np.array([xy_map[i] for i in pick_ids], dtype=float)
+    xorigin, yorigin = xy[:, 0], xy[:, 1]
+
+    x, y = np.copy(xorigin), np.copy(yorigin)
+    x[~fid_flag] += 16.8
+    fid_flag = np.zeros(x.size, dtype=bool)
+    fid_flag[14:] = True
     
     npeaks = x.size
 
