dabf414331800a257bb6272d5783038a5da674a8
wheretogo33 <108924317+wheretogo33@users.noreply.github.com>
2026-01-17 03:16:49 -0500

    Update matchfiber.py

--- kspec_metrology/analysis/matchfiber.py
+++ kspec_metrology/analysis/matchfiber.py
@@ -9,6 +9,9 @@ def matchfiber(x, y
     log = get_logger()
 
     coeff_temp = np.copy(camera2focal_coeff)
+    coeff_temp[1] = np.pi / 12.
+    coeff_temp[2] = 15.
+    coeff_temp[3] = -3.
     xobs, yobs = transform(xobs_raw, yobs_raw, coeff_temp)
 
     nhunt = 720
