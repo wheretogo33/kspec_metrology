896f7bf1fe4f082c89b6bb0f575889fffdf3d15e
wheretogo33 <108924317+wheretogo33@users.noreply.github.com>
2026-01-17 12:51:44 +0900

    Update mtlcal.py

--- kspec_metrology/analysis/mtlcal.py
+++ kspec_metrology/analysis/mtlcal.py
@@ -3,12 +3,9 @@ from kspec_metrology.analysis.findpeak import findpeak
 from kspec_metrology.analysis.matchfiber import matchfiber
 from kspec_metrology.analysis.fitdistortion import fitdistortion
 from astropy.io import ascii
+import json
 
 def mtlcal(data_dir='./MTL/data/'):
-    # Load Fiber positions and Fiducial flag------------------------------------------------
-    #x, y, fid_flag = np.load(data_dir+"pos.npy")
-    #fid_flag = fid_flag.astype(bool)
-    
     tab = ascii.read('Fiber_Configuration_250415.txt')
     tab.rename_columns(tab.colnames[:4], ["ID", "X", "Y", "FiducialFlag"])
     
@@ -29,14 +26,23 @@ def mtlcal(data_dir='./MTL/data/'):
     xy_map = {r["ID"]: (r["X"], r["Y"]) for r in tab}
     xy = np.array([xy_map[i] for i in pick_ids], dtype=float)
     xorigin, yorigin = xy[:, 0], xy[:, 1]
-
+    
     x, y = np.copy(xorigin), np.copy(yorigin)
-    x[~fid_flag] += 16.8
     fid_flag = np.zeros(x.size, dtype=bool)
     fid_flag[14:] = True
     
-    npeaks = x.size
+    #---------------------------------------------------------------
+    x[~fid_flag] += 16.8
+    
+    #---------------------------------------------------------------
+    with open('./target/object.info', 'r') as ff:
+        target = json.load(ff)
+        xall, yall = target['x'], target['y']
+    x[:14], y[:14] = xall[:14], yall[:14]
 
+
+    #---------------------------------------------------------------
+    npeaks = x.size
     _, xobs, yobs = findpeak(npeaks)
 
     imatch, theta_guess = matchfiber(x, y, xobs, yobs)
